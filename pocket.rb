dfe6b80d5ea19935e02bc8b319364a89a0e24231
nelcolon <ncolonmeran@gmail.com>
2020-03-03 14:11:45 -0400

    Changing file name from pocketcore to pocket and updating the release tar link file

--- pocket.rb
+++ pocket.rb
@@ -1,8 +1,8 @@
-class Pocketcore < Formula
+class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.0.1.tar.gz"
-  sha256 "c6ed5588c50d5eb76f32718625e7e69dd19ed3a14b84905ce4c2748ad502fbfc"
+  url "https://github.com/pokt-network/pocket-core/archive/RC-0.1.0.tar.gz"
+  sha256 "aa99a58214da0c095bdfca2b54a0f5849db71a5f3ae60e0c32dfe7d2cdc58bfd"
   depends_on "go" => :build
 
   def install
@@ -15,11 +15,11 @@ class Pocketcore < Formula
       # Install the compiled binary into Homebrew's `bin` - a pre-existing
       # global variable
       system "go", "mod", "vendor"
-      system "go", "build", "-o", bin/"pocket-core", "./app/cmd/pocket_core/main.go"
+      system "go", "build", "-o", bin/"pocket", "./app/cmd/pocket_core/main.go"
     end
   end
 
   test do
-    shell_output("#{bin}/pocket-core", "help")
+    shell_output("#{bin}/pocket", "help")
   end
 end
