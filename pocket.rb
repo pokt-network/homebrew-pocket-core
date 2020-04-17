7b36ff3ac6320e64c2ae68c11853752d04119f58
nelcolon <ncolonmeran@gmail.com>
2020-04-17 17:45:51 -0400

    Updating RC to 0.2.2

--- pocket.rb
+++ pocket.rb
@@ -2,7 +2,7 @@ class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
   url "https://github.com/pokt-network/pocket-core/archive/RC-0.2.2.tar.gz"
-  sha256 "ce159eeedcfa576d7a729e44bf486c62a562d636632561715ff0c4f9fc7567d6"
+  sha256 "813940371da1dbfb7ae28801a3a5c350230a6e809c83823e12f12e494e13f097"
   depends_on "go" => :build
 
   def install
