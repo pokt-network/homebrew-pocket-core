43fecadae78e0688ed88b1abea844aa7599a4465
julio-jimenez <juliofrancisj@gmail.com>
2020-05-09 10:56:46 -0400

    patch 0.3.1

--- pocket.rb
+++ pocket.rb
@@ -1,8 +1,8 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.3.0.tar.gz"
-  sha256 "fd33ffab6995937b39cf3899578d4feb4079091b905717db9047fb27f36573e1"
+  url "https://github.com/pokt-network/pocket-core/archive/RC-0.3.1.tar.gz"
+  sha256 "00b6ac6b55b4eb50d41e94b310b3c2e329c95df5c2ed51259030b9171c94ecba"
   depends_on "go" => :build
   depends_on "leveldb" => :build
 
