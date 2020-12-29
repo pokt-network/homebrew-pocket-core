696cf024c889ff797f9e249b72de8e9b7070307e
julio-jimenez <juliofrancisj@gmail.com>
2020-12-29 14:45:11 -0400

    update RC-0.5.2.9

--- pocket.rb
+++ pocket.rb
@@ -1,13 +1,8 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-<<<<<<< HEAD
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.5.2.tar.gz"
-  sha256 "bc1eecace4ec65afcc9f4e006abf91be54d32ecf29cfa78570de06c1e043a60f"
-=======
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.5.1.tar.gz"
-  sha256 "5409250873668d913f78e7ec46e757cbec1bf362ee72820c298607b93b16a731"
->>>>>>> 766e2fb343ae2800334aa61b34c426c25a901fcb
+  url "https://github.com/pokt-network/pocket-core/archive/RC-0.5.2.9.tar.gz"
+  sha256 "a25f27abb554ba8d6dd65bcdce106886bdcc934119a86049598be4b00f4e307e"
   depends_on "go@1.13" => [:build, "1.13.15"]
   depends_on "leveldb" => :build
 
