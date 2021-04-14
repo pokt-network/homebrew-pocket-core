96b9c35deea9f432cc2f0e0203ccfeb0075d065c
julio-jimenez <juliofrancisj@gmail.com>
2021-04-14 16:36:35 -0400

    Update to RC-0.6.1

--- pocket.rb
+++ pocket.rb
@@ -1,8 +1,8 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.6.0.tar.gz"
-  sha256 "5f299284c5a05c7aa05026c0d5743a1e9c5bb103296a2d28353ec068f7c62246"
+  url "https://github.com/pokt-network/pocket-core/archive/RC-0.6.1.tar.gz"
+  sha256 "8f96bcea3e63d01b68eb280a3071f032ba755681ba0a5578e3c4e5cd33525aee"
   depends_on "go@1.16" => [:build, "1.16.2"]
   depends_on "leveldb" => :build
 
