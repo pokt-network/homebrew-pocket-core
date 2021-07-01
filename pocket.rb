628488e3e0b0ba1689d2a1e5af889fb127ebdc5b
aqt01 <lowell.abbott@gmail.com>
2021-07-01 12:20:38 -0400

    Adds new ver of pocket RC-0.6.3.3

--- pocket.rb
+++ pocket.rb
@@ -1,8 +1,8 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.6.3.2.tar.gz"
-  sha256 "f063a44860b65a6f06d0046de3ef93181de5947f8da5be68476a308c3744f1e8"
+  url "https://github.com/pokt-network/pocket-core/archive/RC-0.6.3.3.tar.gz"
+  sha256 "94c4f3623545fb27281ab395e616d4ba2859f1dee1cd2ce116aad7e4428efe2c"
   depends_on "go@1.16" => [:build, "1.16.2"]
   depends_on "leveldb" => :build
 
