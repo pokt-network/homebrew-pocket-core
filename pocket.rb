edd8850537d55b4943fe584fb1b92f82077c432c
aqt01 <lowell.abbott@gmail.com>
2021-10-11 22:36:58 -0400

    Add beta latest release

--- pocket.rb
+++ pocket.rb
@@ -1,7 +1,7 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.6.3.6.tar.gz"
+  url "https://github.com/pokt-network/pocket-core/archive/BETA-0.6.3.8.tar.gz"
   sha256 "2439e3e798c390261169074469fea8430ea7fc6e7e39a7f5057698d5d6f773c6"
   depends_on "go@1.16" => [:build, "1.16.2"]
   depends_on "leveldb" => :build
