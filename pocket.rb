9d241b79e319b1da991953e9d160e7c811a6c979
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-07-10 12:34:42 -0700

    Bump brew version to 0.10.4

--- pocket.rb
+++ pocket.rb
@@ -1,8 +1,8 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.10.3.tar.gz"
-  sha256 "97d961c727c564fc4649acffd8295ae498133854cff48ef2b62245211fb93e4b"
+  url "https://github.com/pokt-network/pocket-core/archive/RC-0.10.4.tar.gz"
+  sha256 "441948f3d37e5c077f418787f430aa0e6d1ad78ff3e9a0c4c80f428a2caf1320"
   depends_on "go@1.18" => [:build, "1.18"]
   depends_on "leveldb" => :build
 
