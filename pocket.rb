f9551ce5d96b1549be3235c3fd4a7bb338ac36b4
Daniel Olshansky <olshansky.daniel@gmail.com>
2024-01-22 20:48:40 -0500

    Ran ./update_brew.sh BETA-0.11.1

--- pocket.rb
+++ pocket.rb
@@ -1,8 +1,8 @@
 class Pocket < Formula
   desc "Official implementation of the Pocket Network Protocol"
   homepage "https://www.pokt.network"
-  url "https://github.com/pokt-network/pocket-core/archive/RC-0.10.4.tar.gz"
-  sha256 "441948f3d37e5c077f418787f430aa0e6d1ad78ff3e9a0c4c80f428a2caf1320"
+  url "https://github.com/pokt-network/pocket-core/archive/BETA-0.11.1.tar.gz"
+  sha256 "10d996f1a36f13f1eeb5b5a84eb5a71b207351bbbd69f8c8542ce2cf5d8dc306"
   depends_on "go@1.18" => [:build, "1.18"]
   depends_on "leveldb" => :build
 
